515744c0ce1ef00c28e37fde558530da688722b4
Isaque Menezes da Silva <37957188+isaquemenezes@users.noreply.github.com>
2021-10-23 12:12:33 -0300

    update skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,8 +31,22 @@ function linkAction(){
 navLink.forEach(x => x.addEventListener('click', linkAction, false));
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName("skills__content"),
+      skillsHeader  = document.querySelectorAll(".skills__header");
 
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
 
+    for(let i = 0; i< skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+    if(itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+skillsHeader.forEach((elem) => {
+    elem.addEventListener('click', toggleSkills, false);
+});
 /*==================== QUALIFICATION TABS ====================*/
 
 
