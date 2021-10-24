e781a51b9e99726f5bce2b72ac9f975db3a5aa73
Isaque Menezes da Silva <37957188+isaquemenezes@users.noreply.github.com>
2021-10-23 22:26:45 -0300

    Qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,28 @@ function toggleSkills() {
 skillsHeader.forEach((elem) => {
     elem.addEventListener('click', toggleSkills, false);
 });
+
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]'); 
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContent.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active');
+        });
+        target.classList.add('qualification__active');
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active');
+        });
+        tab.classList.add('qualification__active');
+    });
+});
+
+
 
 
 /*==================== SERVICES MODAL ====================*/
